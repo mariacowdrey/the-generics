e69a59a15199062bfeca8a92c238de8f6969453b
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-08-27 18:20:42 -0700

    remove item and update cart

---
+++ store.js
@@ -0,0 +1,33 @@
+// check if document is done loading
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded')
+}
+
+// remove item from cart
+var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemButtons)
+for (var i = 0; i < removeCartItemButtons.length; i++){
+    var button =  removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
+
+    })
+}
+
+// update the total price in cart
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows =  cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
