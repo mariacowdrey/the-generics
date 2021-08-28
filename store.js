076172c399e9dad3752aa3cc85ed920b838b6221
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-08-27 19:07:05 -0700

    check quantity changed

--- store.js
+++ store.js
@@ -1,20 +1,42 @@
 // check if document is done loading
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded')
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
 }
 
 // remove item from cart
-var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-console.log(removeCartItemButtons)
-for (var i = 0; i < removeCartItemButtons.length; i++){
-    var button =  removeCartItemButtons[i]
-    button.addEventListener('click', function(event) {
-        var buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
-        updateCartTotal()
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    console.log(removeCartItemButtons)
+    for (var i = 0; i < removeCartItemButtons.length; i++){
+        var button =  removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
 
-    })
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
 }
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+// check quantity is a number and greater than or equal to zero
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+
+}
+
 
 // update the total price in cart
 function updateCartTotal() {
