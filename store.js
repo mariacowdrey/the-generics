20835f97ee024a1387f2f966de29428b40eee30f
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-08-28 12:05:49 -0700

    add items to cart

--- store.js
+++ store.js
@@ -15,9 +15,15 @@ function ready() {
     }
 
     var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for (var i = 0; i < quantityInputs.length; i++) {
+    for (var i = 0; i < quantityInputs.length; i++){
         var input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons =  document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++){
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
     }
 }
 
@@ -34,9 +40,36 @@ function quantityChanged(event) {
         input.value = 1
     }
     updateCartTotal()
-
 }
 
+// add items to cart
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    console.log(title, price, imageSrc)
+    addItemToCart(title, price, imageSrc)
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartRowContents = `     
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+}
 
 // update the total price in cart
 function updateCartTotal() {
@@ -51,5 +84,7 @@ function updateCartTotal() {
         var quantity = quantityElement.value
         total = total + (price * quantity)
     }
+    // round total to nearest two decimal places
+    total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
